84858869d6bf866040b66548a67a6d65f1866ad1
Salmoor Nurzhanov <salmor662@gmail.com>
2023-03-31 17:28:29 +0600

    Initial commit

---
+++ src/main/java/com/example/controlwork7/ControlWork7Application.java
@@ -0,0 +1,13 @@
+package com.example.controlwork7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlWork7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlWork7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/controlwork7/ControlWork7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlwork7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlWork7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
